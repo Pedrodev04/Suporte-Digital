4a9da9d30184d3196533eee13f725e44f4616760
Pedrodev04 <pedrooodev@gmail.com>
2025-01-29 14:29:35 -0300

    Implementando compatibilidade para versão mobile e animação com scroll raveal com JavaScript.

---
+++ suporte.js
@@ -0,0 +1,59 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.interface',{
+    
+    duration: 1000,
+    distance: '50px'
+})
+revelar.reveal('.contrato',{
+    duration:2000,
+    distance:'90px',
+    delay:700
+})
+revelar.reveal('.skils',{
+    duration:2000,
+    distance: '90px',
+    delay: 300
+})
+revelar.reveal('.missao', {
+    duration:2000,
+    distance:'50px'
+})
+revelar.reveal('.valores1',{
+    duration:2000,
+    distance:'50px'
+})
+revelar.reveal('.valores2',{
+    duration:2000,
+    distance:'50px',
+    delay:100
+})
+revelar.reveal('.valores3',{
+    duration:2000,
+    distance:'50px',
+    delay:200
+})
+revelar.reveal('.valores4',{
+    duration:2000,
+    distance:'50px',
+    delay:300
+})
+revelar.reveal('.cta',{
+    duration:2000,
+    distance:'50px',
+    delay:400
+})
+revelar.reveal('.conteudo1', {
+    duration:2000,
+    distance:'50px'
+})
+revelar.reveal('.conteudo2', {
+    duration:2000,
+    distance:'50px',
+    delay:200
+})
+revelar.reveal('.conteudo3', {
+    duration:2000,
+    distance:'50px',
+    delay:300
+})
